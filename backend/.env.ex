72cd113702c44efc6567737bd79f0782cd8a4396
Amine1s <166218987+Amine1s@users.noreply.github.com>
2026-08-26 12:49:16 +0100

    Add .env.ex with Firebase credentials
    Add Firebase Firestore credentials for serverless functions.

---
+++ backend/.env.ex
@@ -0,0 +1,5 @@
+# Firebase Firestore Server-side Credentials (for Vercel Serverless Functions)
+FIREBASE_PROJECT_ID=unique-diode-x6tp2
+FIREBASE_API_KEY=AIzaSyDDuLTnCJKum7xQhYyLDS7posYk5mWXC0g
+FIRESTORE_DATABASE_ID=ai-studio-luxuryelectronic-9b205320-d69d-4896-853f-dba9e26cdb6e
+FIREBASE_APP_ID=1:613282200427:web:8a5d512f2ac78245faea2e
